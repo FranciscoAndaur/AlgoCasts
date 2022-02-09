6619d4edf898014c18c6ef23aa87b3e28ca08b23
FranciscoAndaur <francisco.andaur@icloud.com>
2022-02-09 14:04:39 -0500

    reversed string with tests passed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    /* SOLUTION 1 */
+    // const result = str.split('');
+    // result.reverse();
+    // return result.join('');
 
+    /* SOLUTION 2 */
+    // return str
+    // .split('')
+    // .reverse()
+    // .join('');
+
+    /* SOLUTION 3 */
+    //Create an empty string called 'reversed'
+     let reversed  = '';
+     //for each character in the provided string
+     for (let character of str) {
+        //Take the character and add it to the start of reversed
+        reversed = character + reversed;
+     }
+    //return the variable 'reversed'
+    return reversed
+
+}
 module.exports = reverse;
